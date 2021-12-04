3cd5be83fed40295f26006580e2693e5dae035d6
pjmicolet <p.micolet@gmail.com>
2021-12-04 11:25:47 +0000

    Did part 2 of day2

--- 2021/day2.cpp
+++ 2021/day2.cpp
@@ -3,9 +3,10 @@
 #include "utils/input_parser.h"
 
 struct pos {
-	pos() : depth(0), position(0) {}
+	pos() : depth(0), position(0), aim(0) {}
 	int depth;
 	int position;
+	int aim;
 };
 
 int main(int argc, char** argv ) {
@@ -26,6 +27,29 @@ int main(int argc, char** argv ) {
 			position.depth+=amount;
 		else if( direc == "up" )
 			position.depth-=amount;
+		splitLine.clear();
+	}
+
+	std::cout << (position.position*position.depth) << "\n";
+
+	std::string filename2 = argv[2];
+	InputParser parser2{filename};
+	position.depth=0;
+	position.position=0;
+	
+	while(parser2.readLine(splitLine, ' '))
+	{
+		auto direc = splitLine[0];
+		int amount = std::stoi(splitLine[1]);
+		if( direc == "forward" )
+		{
+			position.position+=amount;
+			position.depth+= (position.aim*amount);
+		}
+		else if( direc == "down" )
+			position.aim+=amount;
+		else if( direc == "up" )
+			position.aim-=amount;
 		splitLine.clear();
 	}
 
